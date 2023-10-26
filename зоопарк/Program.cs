677c3ac7b20403247d4a5ca830d83c5efcecc995
Eketrin <katebaiko@yandex.ru>
2023-10-26 11:32:31 +0300

    готово, но нужно привести к божескому виду

--- зоопарк/Program.cs
+++ зоопарк/Program.cs
@@ -38,26 +38,56 @@ namespace зоопарк
         public int Number { get; set; }
         public int Size { get; set; }
         public int MaxSumOfAnimals { get; set; }
-        public int SumOfAnimalsNow { get; set; }
-        public HouseOfAnimal(string Name, int Number, int Size, int MaxSumOfAnimals, int SumOfAnimalsNow)
+        public int SumOfAnimalsNow {  get; set; }
+        
+        public HouseOfAnimal(string Name, int Number, int Size, int MaxSumOfAnimals)
         {
             this.Name = Name;
             this.Number = Number;
             this.Size = Size;
             this.MaxSumOfAnimals = MaxSumOfAnimals;
-            this.SumOfAnimalsNow = SumOfAnimalsNow;
+            
 
         }
         public List<Animal> animal = new List<Animal>();
         public void addAnimal(Animal bbb)
         {
-            animal.Add(bbb);
+
+            if (animal.Count==0)
+            {
+                animal.Add(bbb);
+            }
+            else if (bbb.NameOfAnimal == animal[0].NameOfAnimal)
+            {
+                animal.Add(bbb);
+            }
+            else 
+            {
+                Console.WriteLine("Животное нельзя поместить в этот вольер");
+            }
+            SumOfAnimalsNow = animal.Count;
+        }
+        public void DelAnimal(int ccc) //удалить животное
+        {
+            if ((ccc - 1) < animal.Count)
+            {
+                animal.RemoveAt(ccc-1);
+            }
+            else
+            {
+                Console.WriteLine("Такого животного нет");
+            }
+        }
+        public void TransferAnimal( HouseOfAnimal outCletka, HouseOfAnimal innerCletka, int ccc) //перенос животного
+        {
+            innerCletka.addAnimal(outCletka.animal[ccc - 1]);
+            outCletka.DelAnimal(ccc);
         }
         public string WhichAnimal(int chis) //вывод кто в клетке
         {
-            if ((chis - 1) < animal.Count)
+            if ((chis -1) < animal.Count)
             {
-                return animal[chis - 1].NameOfAnimal;
+                return animal[chis-1].NameOfAnimal;
             }
             else
             {
@@ -160,11 +190,18 @@ namespace зоопарк
         static void Main(string[] args)
         {
             Bird animal = new Bird("Какаду", false, 20);
+            Bird animal2 = new Bird("Какаду", false, 15);
             animal.GenerateADescription();
             Zoo zoo = new Zoo("esdsad");
-            HouseOfAnimal cage1 = new HouseOfAnimal("птицы", 21, 55, 15, 5);
+            HouseOfAnimal cage1 = new HouseOfAnimal("птицы", 21, 55, 15);
             zoo.AddCage(cage1);
+            HouseOfAnimal cage2 = new HouseOfAnimal("птицы2", 21, 55, 15);
+            zoo.AddCage(cage2);
             cage1.addAnimal(animal);
+            cage1.addAnimal(animal2);
+            //cage1.TransferAnimal(cage1, cage2, 1);
+            //Console.WriteLine(cage2.animal[0].NameOfAnimal);
+            Console.WriteLine(cage1.SumOfAnimalsNow);
             Console.WriteLine(cage1.WhichAnimal(1));
 
             Console.ReadLine();
